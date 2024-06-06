c85e6237b35bbf3c2bbf61ec818f15e6784c5b29
Abdullah Barakat <abdallah771363885@gmail.com>
2024-06-06 18:24:56 +0300

    BREAKING CHANGE: add pnpm add @generouted/react-router react-router-dom for generative router for cleaner code

--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { PrimeReactProvider } from "primereact/api";
-import { RouterProvider } from "react-router-dom";
+import {
+  BrowserRouter,
+  //  RouterProvider,
+  useRoutes,
+} from "react-router-dom";
 import "virtual:uno.css";
-import "./styles/index.css";
-import router from "./router";
+import "./styles/index.scss";
+// import router from "./router";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./config/queryClient";
-
 
 export function Fallback() {
   return <p>Performing initial data load</p>;
 }
 
+import { Routes } from "@generouted/react-router";
+
 ReactDOM.createRoot(document.getElementById("root") as Element).render(
   <React.StrictMode>
     <PrimeReactProvider>
-     {/* Provide the client to the App */}
-    <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router}  />
+      {/* Provide the client to the App */}
+      <QueryClientProvider client={queryClient}>
+        {/* <RouterProvider router={router} /> */}
+        <Routes />
       </QueryClientProvider>
     </PrimeReactProvider>
   </React.StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -2,24 +2,26 @@ import { defineConfig } from 'vite'
 import UnoCSS from 'unocss/vite'
 import react from '@vitejs/plugin-react'
 import AutoImport from 'unplugin-auto-import/vite'
+import generouted from '@generouted/react-router/plugin'
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve: { alias: { '@': '/src' } },
-  server: {
-    proxy: { 
-      "/api": {
-        target: "https://dummyjson.com",
-        changeOrigin: true,
-        secure:false,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      }
-    }
-  },
+  // server: {
+  //   proxy: { 
+  //     "/api": {
+  //       target: "./src/data/data.json",
+  //       changeOrigin: true,
+  //       secure:false,
+  //       rewrite: (path) => path.replace(/^\/api/, ''),
+  //     }
+  //   }
+  // },
   plugins: [
     react(),
     UnoCSS(),
+    generouted(),
     AutoImport(
 {
   imports:[{
